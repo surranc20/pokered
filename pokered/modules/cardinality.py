284df3f5da1ed7040faa8188f77ab28e15584d0b
csurran2 <surranc20@mail.wlu.edu>
2019-09-28 18:28:46 -0400

    Player sprite reacts to keyboard input

--- pokered/modules/cardinality.py
+++ pokered/modules/cardinality.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 class Cardinality(Enum):
+    """Simple Enumeration of Cardinality that helps to track character orientation"""
     NORTH = 1
     SOUTH = 0
     EAST = 2
-    WEST = 2
+    WEST = -2
