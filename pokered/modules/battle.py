eb26ebb8d2d3fdbf21e83404733ed879c53b35a4
csurran2 <surranc20@mail.wlu.edu>
2019-10-03 14:29:29 -0400

    Added music and battle menu
    Former-commit-id: d46c9548d4e694ad136cc448f198bd3717683a50

--- pokered/modules/battle.py
+++ pokered/modules/battle.py
@@ -1,5 +1,6 @@
 from .vector2D import Vector2
 from .drawable import Drawable
+from os.path import join
 import pygame
 
 class Battle:
@@ -9,10 +10,14 @@ class Battle:
         self._opponent = opponent
         self._draw_surface = draw_surface
         self._battle_background = Drawable("battle_background.png", Vector2(0,0), offset= (0,0))
+        self._battle_menus = Drawable("battle_menus.png", Vector2(0,113))
+        pygame.mixer.music.load(join("music", "gym_battle_music.mp3"))
+        pygame.mixer.music.play(-1)
         
     
     def draw(self):
         self._battle_background.draw(self._draw_surface)
+        self._battle_menus.draw(self._draw_surface)
 
 
 
