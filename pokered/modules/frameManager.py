983c9667e41627f764b77bff94b4065c57432494
csurran2 <surranc20@mail.wlu.edu>
2019-10-08 14:27:39 -0400

    Testing out health bars

--- pokered/modules/frameManager.py
+++ pokered/modules/frameManager.py
@@ -30,7 +30,8 @@ class FrameManager(object):
          join("battle" ,"battle_background.png") : (240, 112),
          join("battle" ,"battle_menus.png") : (240, 48),
          join("battle", "trainer_toss_anim.png") : (64, 64),
-         join("battle", "pokeball_anim.png") : (16, 16)
+         join("battle", "pokeball_anim.png") : (16, 16),
+         join("battle", "health_bars.png") : (108, 42)
 
       }
       
@@ -41,7 +42,7 @@ class FrameManager(object):
       _TRANSPARENCY = [join("battle" ,"battle_menus.png")]
       
       # A list of images that require to be loaded with a color key
-      _COLOR_KEY = ["trainer.png", join("battle", "trainer_toss_anim.png"), join("battle", "pokeball_anim.png"), join("pokemon", "pokemon_big.png")]
+      _COLOR_KEY = ["trainer.png", join("battle", "trainer_toss_anim.png"), join("battle", "pokeball_anim.png"), join("pokemon", "pokemon_big.png"), join("battle", "health_bars.png")]
       
       
       
