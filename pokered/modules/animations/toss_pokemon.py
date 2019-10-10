67a2bffb7108e577fd868444e8aa643180ba8abf
csurran2 <surranc20@mail.wlu.edu>
2019-10-10 16:17:21 -0400

    Added enemy ball toss
    Former-commit-id: 48be2df639c499b56b4c38dc495ed64b25d964f8

--- pokered/modules/animations/toss_pokemon.py
+++ pokered/modules/animations/toss_pokemon.py
@@ -7,6 +7,11 @@ from ..vector2D import Vector2
 
 
 class TossPokemon(AnimatedGroup):
-    def __init__(self, pokemon_name, lead_off=False):
-        if lead_off: super().__init__([TrainerToss(Vector2(50, 48), 0), BallToss(Vector2(25,70), 1), PokeEmerge(Vector2(48, 55), pokemon_name, 2)], Pokemon(pokemon_name))
-        else: super().__init__([BallToss(Vector2(25,70), 1), PokeEmerge(Vector2(48, 55), pokemon_name, 2)], Pokemon(pokemon_name))
+    
+    def __init__(self, pokemon_name, lead_off=False, enemy=False):
+        if lead_off and not enemy: super().__init__([TrainerToss(Vector2(50, 48), 0), BallToss(Vector2(25,70), 1), PokeEmerge(Vector2(48, 55), pokemon_name, 2)], Pokemon(pokemon_name))
+        elif not lead_off and not enemy: 
+            print("here!!!!")
+            super().__init__([BallToss(Vector2(25,70), 0), PokeEmerge(Vector2(48, 55), pokemon_name, 1)], Pokemon(pokemon_name))
+        elif lead_off and enemy: pass
+        else: super().__init__([BallToss(Vector2(165,61),0, enemy=True), PokeEmerge(Vector2(164, 13), pokemon_name, 1, enemy=True)], Pokemon(pokemon_name, enemy=True))
