ddf6d60a2117a1f06a4952128905b73edee68f6a
csurran2 <surranc20@mail.wlu.edu>
2019-10-10 21:16:39 -0400

    Added gender to pokemon and displays

--- pokered/modules/battle_menus/poke_info.py
+++ pokered/modules/battle_menus/poke_info.py
@@ -15,10 +15,10 @@ class PokeInfo(Drawable):
         else:
             super().__init__(join("battle", "health_bars.png"), Vector2(7, 10), offset=(0,0))
         
-        self._blit_name()
+        self._blit_name_and_gender()
         
 
-    def _blit_name(self):
+    def _blit_name_and_gender(self):
         start_pos = Vector2(10,7) if self._enemy else Vector2(20,7)
         current_pos = start_pos
         for char in self._pokemon.get_nick_name().lower():
@@ -28,4 +28,16 @@ class PokeInfo(Drawable):
             print(font_char)
             self._image.blit(font_char, (current_pos.x, current_pos.y))
             current_pos.x += 5
+        
+        current_pos.x += 2
+        if self._pokemon.get_gender() == "male":
+            font_index = 11
+            font_char = FRAMES.getFrame("pokemon_fire_red_battle_font.png", offset=(font_index, 3))
+            self._image.blit(font_char, (current_pos.x, current_pos.y))
+        else:
+            font_index = 12
+            font_char = FRAMES.getFrame("pokemon_fire_red_battle_font.png", offset=(font_index, 3))
+            self._image.blit(font_char, (current_pos.x, current_pos.y))
+
+        
             
