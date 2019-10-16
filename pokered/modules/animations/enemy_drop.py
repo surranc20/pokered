193967335b993d555bb1e94e86d78c3cd12c9624
csurran2 <surranc20@mail.wlu.edu>
2019-10-16 14:19:24 -0400

    Add comments to animation files

--- pokered/modules/animations/enemy_drop.py
+++ pokered/modules/animations/enemy_drop.py
@@ -5,12 +5,16 @@ from ..vector2D import Vector2
 
 class EnemyDrop(AnimatedGroupPart):
     def __init__(self, position, anim_sequence_pos):
+        """"This is enemies equivalent of a TrainerToss. The enemy simply moves right off the screen"""
         super().__init__(join("battle", "gary_battle.png"), position, anim_sequence_pos)
         self._nFrames = 1
         self._framesPerSecond = 1
         self._initial_position = position
 
     def update(self, ticks):
+        """Player moves two pixels per update method. Returns the index of the next AnimatedGroupPart when done.
+        In this case that Will always be BallToss."""
+        #TODO: Maybe change this so that it utilizes ticks to determine how far to move enemy?
         self.setPosition(self.getPosition() + Vector2(2, 0))
         if self.getPosition().x > self._initial_position.x + 4:
             if self.getPosition().x > 240: self.kill()
