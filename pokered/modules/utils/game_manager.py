4e3a11b717fe1d026b54ff8ca122bc7daf81f103
csurran2 <surranc20@mail.wlu.edu>
2019-11-11 11:12:04 -0500

    Normalized tab spacing

--- pokered/modules/utils/game_manager.py
+++ pokered/modules/utils/game_manager.py
@@ -4,19 +4,20 @@ from .FSMs.gameFSM import GameState
 
 
 class GameManager(object):
-   def __init__(self, screenSize):
-      self._level = LevelManager()
-      self._FSM = "running" #This is a temporary hack. Do not know if I will need FSM.
+    def __init__(self, screenSize):
+        self._screen_size = screen_size
+        self._level = LevelManager()
+        self._FSM = "running" #This is a temporary hack. Do not know if I will need FSM.
 
-   def draw(self, surface):
-      if self._FSM == "running":
-         self._level.draw(surface)
+    def draw(self, surface):
+        if self._FSM == "running":
+            self._level.draw(surface)
    
-   def handleEvent(self, event):
-    if self._FSM == "running": 
-        self._level.handleEvent(event)
+    def handle_event(self, event):
+        if self._FSM == "running": 
+            self._level.handle_event(event)
     
-   def update(self, ticks, screenSize):
-      if self._FSM == "running":
-         self._level.update(ticks, screenSize)
+    def update(self, ticks):
+        if self._FSM == "running":
+            self._level.update(ticks, self._screen_size)
 
