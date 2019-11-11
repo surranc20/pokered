8835a8ba932780491bd174794592b246484ae636
csurran2 <surranc20@mail.wlu.edu>
2019-11-11 11:12:04 -0500

    Normalized tab spacing
    Former-commit-id: 4e3a11b717fe1d026b54ff8ca122bc7daf81f103

--- pokered/modules/utils/level_manager.py
+++ pokered/modules/utils/level_manager.py
@@ -2,15 +2,15 @@ from .vector2D import Vector2
 from .drawable import Drawable
    
 class LevelManager(object):
-   def __init__(self, player):
-      self._player = player
+    def __init__(self, player):
+        self._player = player
 
-   def draw(self, surface):
-      surface.fill((30,30,30))
+    def draw(self, surface):
+        surface.fill((30,30,30))
       
-   def handle_event(self, event):
-      self._player.handleEvent(event)
+    def handle_event(self, event):
+        self._player.handleEvent(event)
       
-   def update(self, ticks, screenSize):
-      self._star.update(ticks, LevelManager._WORLD_SIZE)
-      Drawable.updateWindowOffset(self._star, screenSize, LevelManager._WORLD_SIZE)
+    def update(self, ticks, screenSize):
+        self._star.update(ticks, LevelManager._WORLD_SIZE)
+        Drawable.updateWindowOffset(self._star, screenSize, LevelManager._WORLD_SIZE)
