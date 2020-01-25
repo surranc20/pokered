7b59be6b8100dbf93e7168a646ad30b6f0314252
surranc20 <surranc20@mail.wlu.edu>
2020-01-25 14:42:59 -0500

    Added help text to menu

--- pokered/modules/events/menu.py
+++ pokered/modules/events/menu.py
@@ -1,8 +1,11 @@
 import pygame
 from os.path import join
+from ..enumerated.battle_actions import BattleActions
 from ..utils.vector2D import Vector2
 from ..utils.text_maker import TextMaker
 from ..utils.managers.frameManager import FRAMES
+from ..utils.UI.drawable import Drawable
+from ..utils.managers.soundManager import SoundManager
 
 
 class Menu():
@@ -13,11 +16,17 @@ class Menu():
         self._save_queued = False
         self._make_menu()
         self._make_menu_text()
+        self._cursor = Cursor(len(self._options))
+        self._make_help_bar()
+        self._make_help_text("hello")
 
     def draw(self, draw_surface):
         """Draws the menu."""
-        draw_surface.blit(self._menu_surface, (180, 10))
-        draw_surface.blit(self._text_surface, (180, 10))
+        draw_surface.blit(self._menu_surface, (172, 10))
+        draw_surface.blit(self._text_surface, (172, 10))
+        draw_surface.blit(self._help_surface, (0, 122))
+        draw_surface.blit(self._help_text_surface, (0, 122))
+        self._cursor.draw(draw_surface)
 
     def update(self, ticks):
         """Updates the menu."""
@@ -30,6 +39,10 @@ class Menu():
             # self._save_queued = True
             self._is_over = True
 
+        elif event.type == pygame.KEYDOWN:
+
+            self._cursor.change_cursor_pos(event)
+
     def is_over(self):
         """Determines whether or not the menu is closed."""
         return self._is_over
@@ -41,7 +54,7 @@ class Menu():
     def _make_menu(self):
         """Creates the menu image."""
         # Create the surface where the menu is drawn
-        self._menu_surface = pygame.Surface((56, 100))
+        self._menu_surface = pygame.Surface((64, 100))
         self._menu_surface.fill((255, 255, 255))
         self._menu_surface.set_colorkey((255, 255, 255))
 
@@ -55,7 +68,8 @@ class Menu():
         self._menu_surface.blit(top_mid, (24, 0))
         self._menu_surface.blit(top_mid, (32, 0))
         self._menu_surface.blit(top_mid, (40, 0))
-        self._menu_surface.blit(top_right, (48, 0))
+        self._menu_surface.blit(top_mid, (48, 0))
+        self._menu_surface.blit(top_right, (56, 0))
 
         # Create the middle rows
         white_blob = FRAMES.getFrame("menu_parts.png", offset=(1, 1))
@@ -63,15 +77,30 @@ class Menu():
         mid_right = FRAMES.getFrame("menu_parts.png", offset=(2, 1))
         white_blob.set_colorkey((30, 40, 10))
 
+        # Look at the players progress and see which menu items should be
+        # displayed given their progress in the game. (i.e has player received
+        # their first pokemon yet...)
+        middle_rows = 7
+        if self._player.pokedex is not None:
+            middle_rows += 1
+        if self._player.has_first_pokemon:
+            middle_rows += 1
+        if middle_rows == 9:
+            middle_rows += 1
+        if middle_rows == 8:
+            middle_rows += 1
+
+        # Create the appropriated number of middle rows
         current_y = 8
-        for i in range(7):
+        for i in range(middle_rows):
             self._menu_surface.blit(mid_left, (0, current_y))
             self._menu_surface.blit(white_blob, (8, current_y))
             self._menu_surface.blit(white_blob, (16, current_y))
             self._menu_surface.blit(white_blob, (24, current_y))
             self._menu_surface.blit(white_blob, (32, current_y))
             self._menu_surface.blit(white_blob, (40, current_y))
-            self._menu_surface.blit(mid_right, (48, current_y))
+            self._menu_surface.blit(white_blob, (48, current_y))
+            self._menu_surface.blit(mid_right, (56, current_y))
             current_y += 8
 
         # Create the bottom rows
@@ -84,34 +113,79 @@ class Menu():
         self._menu_surface.blit(bottom_mid, (24, current_y))
         self._menu_surface.blit(bottom_mid, (32, current_y))
         self._menu_surface.blit(bottom_mid, (40, current_y))
-        self._menu_surface.blit(bottom_right, (48, current_y))
+        self._menu_surface.blit(bottom_mid, (48, current_y))
+        self._menu_surface.blit(bottom_right, (56, current_y))
 
     def _make_menu_text(self):
         """Adds the menu text to the menu"""
         # Create the surface where the text is drawn
-        self._text_surface = pygame.Surface((56, 100))
+        self._text_surface = pygame.Surface((64, 100))
         self._text_surface.fill((255, 255, 255))
         self._text_surface.set_colorkey((255, 255, 255))
 
-        menu_text = ["bag", "PLAYER", "SAVE", "OPTION", "EXIT"]
-        current_pos = Vector2(0, 10)
+        # Grab the appropriate options to display to the player based on thier
+        # progress in the game.
+        menu_text = []
+        if self._player.pokedex is not None:
+            menu_text.append("POKeDEX")
+        if self._player.has_first_pokemon:
+            menu_text.append("POKeMON")
+        menu_text += ["BAG", str(self._player.name), "SAVE", "OPTION", "EXIT"]
+        self._options = menu_text
+
+        # Display the options.
+        current_pos = Vector2(0, 12)
         text_maker = TextMaker(join("fonts", "party_txt_font.png"))
         for text in menu_text:
-            current_pos.x = 12
+            current_pos.x = 15
             word_surf = text_maker.get_surface(text)
             self._text_surface.blit(word_surf, (current_pos.x, current_pos.y))
             current_pos.y += 11
 
+    def _make_help_bar(self):
+        """Create the blue help bar surface"""
+        self._help_surface = pygame.Surface((240, 48))
+        self._help_surface.blit(FRAMES.getFrame("blue_help_box.png"), (0, 0))
+
+    def _make_help_text(self, string):
+        """Create help text"""
+        text_maker = TextMaker(join("fonts", "menu_font.png"))
+        self._help_text_surface = pygame.Surface((240, 48))
+        self._help_text_surface.fill((190, 190, 112))
+        self._help_text_surface.set_colorkey((190, 190, 112))
+        word_surf = text_maker.get_surface(string)
+        self._help_text_surface.blit(word_surf, (10, 5))
 
 
+class Cursor(Drawable):
+    """Small Cursor class that keeps track of the menu cursor."""
+    def __init__(self, max_value):
+        """Initialize the cursor. Max_value is the highest possible value the
+        cursor can have based on the size of the menu."""
+        self._max_value = max_value
+        self.cursor = 0
+        super().__init__(join("battle", "cursor.png"), (178, 20),
+                         world_bound=False)
 
+    def reset(self):
+        """Resets the cursor to its initial position."""
+        self.cursor = 0
+        self._position = (174, 20 + self.cursor * 11)
 
+    def draw(self, draw_surface):
+        """Draws the cursor"""
+        super().draw(draw_surface)
 
-            # for char in text.lower():
-            #     font_index = int(ord(char)) - 97
-            #     font_char = FRAMES.getFrame("party_txt_font.png",
-            #                                 offset=(font_index, 0))
-            #     font_char.set_colorkey((255, 255, 255))
-            #     self._text_surface.blit(font_char, (current_pos.x, current_pos.y))
-            #     current_pos.x += 5
-            # current_pos.y += 11
+    def change_cursor_pos(self, action):
+        """Changes the cursor pos based on the action provided"""
+        if action.key == BattleActions.UP.value:
+            if self.cursor > 0:
+                self.cursor -= 1
+                SoundManager.getInstance().playSound("firered_0005.wav")
+
+        elif action.key == BattleActions.DOWN.value:
+            if self.cursor < self._max_value - 1:
+                self.cursor += 1
+                SoundManager.getInstance().playSound("firered_0005.wav")
+
+        self._position = (178, 20 + self.cursor * 11)
