05a48a14fe31f4bd3670796bb009cb454280b683
surranc20 <surranc20@mail.wlu.edu>
2020-05-04 11:47:37 -0500

    Changed variable type to train_type to avoid overwriting python type

--- pokered/modules/level.py
+++ pokered/modules/level.py
@@ -102,7 +102,7 @@ class Level():
                         battle_dialogue_id=trainer_args[4],
                         post_battle_dialogue_id=trainer_args[5],
                         gender=trainer_args[8],
-                        type=trainer_args[9]
+                        train_type=trainer_args[9]
                         )
 
             # If the trainer has an event specified in the meta data then add
