5b71cf78f8225b17601a1dcdb9571cc72d73e790
surranc20 <surranc20@mail.wlu.edu>
2020-05-19 16:43:29 -0500

    Added talk event to nurse
    Former-commit-id: 592bdb0af08dbb4ad47a507f442829e2453309bb

--- pokered/modules/nurse.py
+++ pokered/modules/nurse.py
@@ -1,10 +1,16 @@
 from .npc import NPC
+from .events.nurse_heal import NurseHeal
 
 
 class Nurse(NPC):
     def __init__(self, position, facing):
         """Creates a nurse object"""
         super().__init__("nurse", position, facing)
+        self.gender = "female"
 
     def update(self, ticks, nearby_tiles, current_tile):
         pass
+
+    def talk_event(self, player):
+        """Returns the nurse "talk event" which heals the player's pokemon"""
+        return NurseHeal(self, player)
