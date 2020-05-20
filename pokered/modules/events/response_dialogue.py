49cb9e8591f0636e644eb85564cb1a028ccab64b
surranc20 <surranc20@mail.wlu.edu>
2020-05-20 15:22:11 -0500

    Response Dialogue actually works now

--- pokered/modules/events/response_dialogue.py
+++ pokered/modules/events/response_dialogue.py
@@ -1,4 +1,11 @@
+import pygame
+from os.path import join
+
 from .dialogue import Dialogue
+from .menu import Cursor
+from ..utils.UI.resizable_menu import ResizableMenu
+from ..utils.text_maker import TextMaker
+from ..enumerated.battle_actions import BattleActions
 
 
 class ResponseDialogue(Dialogue):
@@ -11,17 +18,39 @@ class ResponseDialogue(Dialogue):
         response."""
         super().__init__(dialogue_id, player, npc, box=box, gender=gender)
         self.response = None
-    
+        self.response_menu = None
+
     def _blit_line(self):
         if self._current_line > len(self._dialogue):
             return
         super()._blit_line()
         if self._current_line == len(self._dialogue):
             self._text_cursor.deactivate()
-    
+            self._blit_response()
+
+    def draw(self, draw_surface):
+        super().draw(draw_surface)
+        if self.response_menu is not None:
+            draw_surface.blit(self.response_menu, (150, 70))
+            self.cursor.draw(draw_surface)
+
+    def handle_event(self, event):
+        if self.response_menu is not None:
+            if event.type == pygame.KEYDOWN:
+                if event.key in [BattleActions.UP.value,
+                                 BattleActions.DOWN.value]:
+                    self.cursor.change_cursor_pos(event)
+                elif event.key == BattleActions.SELECT.value:
+                    self.response = self.cursor.cursor
+        else:
+            super().handle_event(event)
+
     def _blit_response(self):
         """Blit the response box as well as the two prompts"""
-        pass
+        self.response_menu = ResizableMenu(2, width=5).menu_surface
+        text_maker = TextMaker(join("fonts", "party_txt_font.png"), max=15, line_height=12)
+        self.response_menu.blit(text_maker.get_surface("YES NO"), (15, 11))
+        self.cursor = Cursor(2, initial_pos=(156, 79))
 
     def is_over(self):
         return self.response is not None
