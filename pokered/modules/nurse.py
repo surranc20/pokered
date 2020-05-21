fce9a718e0f0a431d026e6077a429b31855d155e
surranc20 <surranc20@mail.wlu.edu>
2020-05-21 11:31:33 -0500

    Changed nurse image to correct image

--- pokered/modules/nurse.py
+++ pokered/modules/nurse.py
@@ -5,7 +5,7 @@ from .events.nurse_heal import NurseHeal
 class Nurse(NPC):
     def __init__(self, position, facing):
         """Creates a nurse object"""
-        super().__init__("nurse", position, facing)
+        super().__init__("nurse_joy", position, facing)
         self.gender = "female"
 
     def update(self, ticks, nearby_tiles, current_tile):
