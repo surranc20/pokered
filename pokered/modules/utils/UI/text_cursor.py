d1160814a64513b909e5e60f8d11446223a25a07
surranc20 <surranc20@mail.wlu.edu>
2020-06-09 21:34:28 -0500

    Text cursor can now change the initial direction of the bobbing movement

--- pokered/modules/utils/UI/text_cursor.py
+++ pokered/modules/utils/UI/text_cursor.py
@@ -1,16 +1,17 @@
-import pygame
 from .drawable import Drawable
 
 
 class TextCursor(Drawable):
-    def __init__(self, pos):
-        """A simple text cursor object that bobs up and down while waiting for the user to press enter."""
-        super().__init__("text_cursor.png", pos)
+    def __init__(self, pos, name="text_cursor.png", invert=False):
+        """A simple text cursor object that bobs up and down while waiting for
+        the user to press enter."""
+        super().__init__(name, pos)
         self._is_active = False
-        self._current_delta = 1
+        self._current_delta = 1 if not invert else -1
         self._timer = 0
         self._world_bound = False
         self._anchored_pos = pos
+        self._invert = invert
 
     def activate(self):
         """Activates the cursor so that it can be drawn."""
@@ -34,12 +35,20 @@ class TextCursor(Drawable):
 
     def update(self, ticks):
         """Updates the cursor so that it can bob up and down."""
-        if self._position == self._anchored_pos:
-            self._current_delta = 1
-        elif self._position[1] - self._anchored_pos[1] >= 2:
-            self._current_delta = -1
+        if not self._invert:
+            if self._position == self._anchored_pos:
+                self._current_delta = 1
+            elif self._position[1] - self._anchored_pos[1] >= 2:
+                self._current_delta = -1
+        else:
+            if self._position == self._anchored_pos:
+                self._current_delta = -1
+            elif self._anchored_pos[1] - self._position[1] >= 2:
+                self._current_delta = 1
 
         self._timer += ticks
         if self._timer > .2:
-            self._position = (self._position[0], self._position[1] + self._current_delta)
+            self._position = (self._position[0],
+                              self._position[1] + self._current_delta)
+
             self._timer -= .2
