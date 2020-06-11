d8c30e60f292394fa637a8e9c6af1ea37f850fc1
surranc20 <surranc20@mail.wlu.edu>
2020-06-11 14:47:08 -0500

    Outlien of bag event
    Former-commit-id: cd1ea02ce5bb9ad0c8886183f450d3abd0de0fea

---
+++ pokered/modules/events/menu_events/bag.py
@@ -0,0 +1,19 @@
+class Bag():
+    """The bag menu from the game."""
+    def __init__(self, player):
+        self.player = player
+
+    def is_over(self):
+        """Returns whether or not the bag has been exited."""
+
+    def update(self, ticks):
+        """Updates the bag."""
+        pass
+
+    def handle_event(self, event):
+        """Handles event."""
+        pass
+
+    def draw(self, draw_surface):
+        """Draws the bag."""
+        pass
