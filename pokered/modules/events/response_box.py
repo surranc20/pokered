1e46ea5ada926322cf6621a8dc5e6db36f80ef9f
surranc20 <surranc20@mail.wlu.edu>
2020-06-25 21:38:49 -0500

    Added response box
    Former-commit-id: fc8b184029d8a9f40f9dfab6d52fb5aad45dd247

---
+++ pokered/modules/events/response_box.py
@@ -0,0 +1,45 @@
+from os.path import join
+from ..utils.UI.resizable_menu import ResizableMenu
+from ..utils.text_maker import TextMaker
+from ..utils.cursor import Cursor
+from ..enumerated.battle_actions import BattleActions
+
+
+class ResponseBox():
+    """Class which creates a box of varying dimensions and will display the
+    options as well as a cursor."""
+    def __init__(self, lines, pos, width=8, line_height=None, dx=0, dy=0):
+        self.pos = pos  # Position where menu will be drawn.
+        self.is_dead = False  # Tracks if an option has been selected.
+
+        # Create text maker with the desired height
+        text_maker = TextMaker(join("fonts", "party_txt_font.png"),
+                               line_height)
+
+        # Create the menu
+        self.menu = ResizableMenu(len(lines), width=width).menu_surface
+
+        # Display options on menu
+        y_pos = 12  # Blit first option 12 pixels down
+        for line in lines:
+            self.menu.blit(text_maker.get_surface(line), (16, y_pos))
+            y_pos += text_maker.line_height
+
+        # Create cursor the position of the response box offset by (7, 10)
+        # Dx and Dy can be used for further granularity.
+        self.cursor = Cursor(len(lines), (pos[0] + 7 + dx, pos[1] + 10 + dy))
+
+    def draw(self, draw_surface):
+        """Draws the menu (with the options) and the cursor."""
+        draw_surface.blit(self.menu, self.pos)
+        self.cursor.draw(draw_surface)
+
+    def handle_event(self, event):
+        """Update the cursor or select an option."""
+        if event.key in [BattleActions.UP.value,
+                         BattleActions.DOWN.value]:
+            self.cursor.change_cursor_pos(event)
+
+        elif event.key == BattleActions.SELECT.value:
+            self.response = self.cursor.cursor
+            self.is_dead = True
