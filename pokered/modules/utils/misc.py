5749430c13cedf5c16ece233638df1b52a054507
surranc20 <surranc20@mail.wlu.edu>
2020-06-27 20:33:20 -0500

    end_at now supports height as well

--- pokered/modules/utils/misc.py
+++ pokered/modules/utils/misc.py
@@ -1,4 +1,5 @@
 def end_at(surf, end_coord):
     surf_width = surf.get_width()
-    start_pos = (end_coord[0] - surf_width, end_coord[1])
+    surf_height = surf.get_height()
+    start_pos = (end_coord[0] - surf_width, end_coord[1] - surf_height)
     return start_pos
