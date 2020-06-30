cd25e5c4d1c26a8c97212c9ebb6ea9d3984abfcc
surranc20 <surranc20@mail.wlu.edu>
2020-06-29 21:09:18 -0500

    Added sell event to pokemart

--- pokered/modules/events/poke_mart_event.py
+++ pokered/modules/events/poke_mart_event.py
@@ -2,14 +2,17 @@ import pygame
 from os.path import join
 from .dialogue import Dialogue
 from .response_dialogue import ResponseDialogue
+from .menu_events.bag import Bag
 from ..utils.UI.resizable_menu import ResizableMenu
 from ..utils.UI.drawable import Drawable
+from ..utils.UI.quantity_cursor import QuantityCursor
 from ..utils.text_maker import TextMaker
 from ..utils.managers.frameManager import FRAMES
 from ..utils.misc import end_at
 from ..utils.UI.text_cursor import TextCursor
 from ..utils.cursor import Cursor
 from ..enumerated.battle_actions import BattleActions
+from ..enumerated.item_types import ItemTypes
 
 
 class PokeMartEvent():
@@ -34,8 +37,12 @@ class PokeMartEvent():
                                  self._clerk,
                                  response_string="BUY SELL SEE YA!")
         self._seeya_dialogue = Dialogue("19", self._player, self._clerk)
+
+        # Create the buy and sell menus
         self._buy_menu = PokeMartMenu(self._clerk.inventory, self._player,
                                       self._clerk)
+        
+        self._sell_menu = SellEvent(self._player)
 
         # Create surface which displays how much money the player has.
         self._create_money_surface()
@@ -58,7 +65,7 @@ class PokeMartEvent():
         elif self._response is None:
             self._response = self._initial_prompt.response
 
-        # If the user selected seeya then pass control to said dialogue.
+        # If the user selected seeya then pass control to seeya dialogue.
         elif self._response == 2 and not self._seeya_dialogue.is_over():
             self._seeya_dialogue.update(ticks)
 
@@ -96,6 +103,19 @@ class PokeMartEvent():
                                      response_string="BUY SELL SEE YA!")
             self._buy_menu = PokeMartMenu(self._clerk.inventory, self._player,
                                           self._clerk)
+        
+        elif self._response == 1 and not self._sell_menu.is_over():
+            self._sell_menu.update(ticks)
+        
+        elif self._response == 1 and self._sell_menu.is_over():
+            self._response = None
+            self.turned = False
+            self._sell_menu = SellEvent(self._player)
+            self._initial_prompt = \
+                MartResponseDialogue("21", self._player,
+                                     self._clerk,
+                                     response_string="BUY SELL SEE YA!")
+
 
     def draw(self, draw_surface):
         """Draws the poke mart menu event based on where the user is in the
@@ -109,10 +129,16 @@ class PokeMartEvent():
             draw_surface.blit(self._money_surface, (2, 2))
             draw_surface.blit(self._help_surface, (0, 111))
             self._buy_menu.draw(draw_surface)
+        elif self._response == 1 and not self._sell_menu.is_over():
+            self._sell_menu.draw(draw_surface)
 
     def handle_event(self, event):
         """Passes on the event to the active subevent/menu. Code is ordered
         from first to last in terms of chronological order."""
+        # Get rid of all non keydown events
+        if event.type != pygame.KEYDOWN:
+            return
+
         if not self._initial_prompt.is_over():
             self._initial_prompt.handle_event(event)
 
@@ -125,6 +151,9 @@ class PokeMartEvent():
         # menu.
         elif self._response == 0 and not self._buy_menu.is_over():
             self._buy_menu.handle_event(event)
+        
+        elif self._response == 1 and not self._sell_menu.is_over():
+            self._sell_menu.handle_event(event)
 
     def get_end_event(self):
         """Tells the level manager to return control to the level once the
@@ -610,63 +639,88 @@ class HowManySelector():
         return self.is_dead
 
 
-class QuantityCursor():
-    def __init__(self, pos):
-        """Creates a Quantity Cursor. Shows number selected and the cursor's
-        going up and down."""
-        # Create the two arrow cursors.
-        self.down_arrow_surf = FRAMES.getFrame("shop_menu_cursor.png")
-        # The up arrow needs to be flipped horizontally. Need to make a
-        # separate surface because FRAMES are Singleton. Directly transforming
-        # the frame will also change the down cursor.
-        self.up_arrow_surf = pygame.Surface(self.down_arrow_surf.get_size())
-        self.up_arrow_surf.fill((255, 245, 245))
-        self.up_arrow_surf.set_colorkey((255, 245, 245))
-        self.up_arrow_surf.blit(pygame.transform.flip(self.down_arrow_surf,
-                                                      False, True), (0, 0))
-
-        # Create the position's of the cursors.
-        self.up_pos = pos
-        self.up_anchor = self.up_pos
-        self.down_pos = (pos[0], pos[1] + 25)
-        self.down_anchor = self.down_pos
-
-        # Default pixel amount the cursor changes per change.
-        self.current_delta = 1
-
-        # Timer for cursors.
-        self.timer = 0
-
-        # Create the count surface which displays the amount of items selected.
-        self.text_maker = TextMaker(join("fonts", "party_txt_font.png"))
-        self.count_surf = self.text_maker.get_surface("x01")
-        self.count_pos = (self.up_pos[0] + 1, self.up_pos[1] + 13)
-
-    def change_count(self, count):
-        """Changes the amount displayed on the cursor."""
-        self.count_surf = \
-            self.text_maker.get_surface(f"x{str(count).zfill(2)}")
-
+class SellEvent(Bag):
+    """Sell event triggered a when player wants to sell anything inside of a 
+    pokemon shop."""
+    def __init__(self, player):
+        super().__init__(player)
+        self.active_sell_event = None
+    
     def draw(self, draw_surface):
-        """Draws the two cursors and the amount selected."""
-        draw_surface.blit(self.up_arrow_surf, self.up_pos)
-        draw_surface.blit(self.count_surf, self.count_pos)
-        draw_surface.blit(self.down_arrow_surf, self.down_pos)
-
+        super().draw(draw_surface)
+        if self.active_sell_event is not None:
+            self.active_sell_event.draw(draw_surface)
+    
+    def handle_event(self, event):
+        if self.active_sell_event is not None:
+            self.active_sell_event.handle_event(event)
+        else:
+            super().handle_event(event)
+    
     def update(self, ticks):
-        """Updates the two cursors and changes their positions."""
-        self.timer += ticks
+        if self.active_sell_event is not None:
+            self.active_sell_event.update(ticks)
+            if self.active_sell_event.is_dead:
+                self.is_dead = True
+        super().update(ticks)
+    
+    def _handle_select_event(self):
+        selected_item = self.item_list[self.item_cursor.cursor]
+        if selected_item == "CANCEL":
+            self.is_dead = True
+        elif selected_item.type == ItemTypes.KEY_ITEMS:
+            self.do_what_response_menu = \
+                Dialogue("29", self.player, self.player, 
+                         replace=[selected_item.name.upper()], show_curs=False)
+        else:
+            self.active_sell_event = CanSellSubEvent(self.player,
+                                                     selected_item)
 
-        # Direction that the cursors are going. Anchor points are used to
-        # determine when the cursor needs to change direction.
-        if self.down_pos == self.down_anchor:
-            self.current_delta = 1
-        elif self.down_pos[1] - self.down_anchor[1] >= 2:
-            self.current_delta = -1
 
-        # Cursor only moves 5 times a second.
-        if self.timer > .2:
-            self.up_pos = (self.up_pos[0], self.up_pos[1] - self.current_delta)
-            self.down_pos = (self.down_pos[0],
-                             self.down_pos[1] + self.current_delta)
-            self.timer -= .2
+class CanSellSubEvent():
+    def __init__(self, player, item):
+        self.player = player
+        self.item = item
+        self.quant = 1
+        self.max_quant = player.bag[item.type][item]
+        self.is_dead = False
+
+        # Asks the user how many of the item they want to sell.
+        self.how_many_dialogue = Dialogue("30", self.player, self.player,
+                                          show_curs=False,
+                                          replace=[item.name.upper()])
+
+        # Create the menu frame which shows the quantity cursor and the amount 
+        # the player recieaves for selling a given number of the item.
+        self.text_maker = TextMaker(join("fonts", "party_txt_font.png"))
+        self.menu_frame = ResizableMenu(3, width=14).menu_surface
+        self.quantity_cursor = QuantityCursor((140, 71))
+        self.cost_surf = \
+            self.text_maker.get_surface(str(self.item.sell_price * self.quant))
+    
+    def update(self, ticks):
+        self.quantity_cursor.update(ticks)
+
+    def handle_event(self, event):
+        if event.key == BattleActions.UP.value:
+            if self.quant < self.max_quant:
+                self.update_cursor_and_price(self.quant + 1)
+            elif self.quant == self.max_quant:
+                self.update_cursor_and_price(1)
+        elif event.key == BattleActions.DOWN.value:
+            if self.quant > 1:
+                self.update_cursor_and_price(self.quant - 1)
+            elif self.quant == 1:
+                self.update_cursor_and_price(self.max_quant)
+                
+    def draw(self, draw_surface):
+        self.how_many_dialogue.draw(draw_surface)
+        draw_surface.blit(self.menu_frame, (128, 64))
+        draw_surface.blit(self.cost_surf, end_at(self.cost_surf, (225, 84)))
+        self.quantity_cursor.draw(draw_surface)
+    
+    def update_cursor_and_price(self, new_quant):
+        self.quant = new_quant
+        self.quantity_cursor.change_count(self.quant)
+        self.cost_surf = \
+            self.text_maker.get_surface(str(self.item.sell_price * self.quant))
