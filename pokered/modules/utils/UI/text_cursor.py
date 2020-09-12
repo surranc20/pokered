a6e4a305ed805ffc3849211321cc44e93699e012
surranc20 <surranc20@mail.wlu.edu>
2020-09-12 17:04:46 -0500

    Text cursor can now be stationary
    Former-commit-id: fa61561d127f6682f4b5f6547c4b021f62d41e34

--- pokered/modules/utils/UI/text_cursor.py
+++ pokered/modules/utils/UI/text_cursor.py
@@ -3,10 +3,10 @@ from .drawable import Drawable
 
 class TextCursor(Drawable):
     def __init__(self, pos, name="text_cursor.png", invert=False,
-                 horizontal=False):
+                 horizontal=False, offset=None):
         """A simple text cursor object that bobs up and down while waiting for
         the user to press enter."""
-        super().__init__(name, pos)
+        super().__init__(name, pos, offset=offset)
         self._is_active = False
         self._current_delta = 1 if not invert else -1
         self._timer = 0
@@ -15,6 +15,8 @@ class TextCursor(Drawable):
         self._invert = invert
         self._horizontal = horizontal
 
+        self.should_update = True
+
     def activate(self):
         """Activates the cursor so that it can be drawn."""
         self._is_active = True
@@ -22,6 +24,9 @@ class TextCursor(Drawable):
     def deactivate(self):
         """Deactivates the cursor so that if will not be drawn."""
         self._is_active = False
+
+    def reset(self):
+        self._position = self._anchored_pos
 
     def set_pos(self, pos):
         """Sets the position of the cursor. The parameter expects the position
@@ -39,23 +44,24 @@ class TextCursor(Drawable):
         """Updates the cursor so that it can bob up and down."""
         main_axis = 0 if self._horizontal else 1
 
-        if not self._invert:
-            if self._position == self._anchored_pos:
-                self._current_delta = 1
-            elif self._position[main_axis] - self._anchored_pos[main_axis] >= 2:
-                self._current_delta = - 1
-        else:
-            if self._position == self._anchored_pos:
-                self._current_delta = - 1
-            elif self._anchored_pos[main_axis] - self._position[main_axis] >= 2:
-                self._current_delta = 1
+        if self.should_update:
+            if not self._invert:
+                if self._position == self._anchored_pos:
+                    self._current_delta = 1
+                elif self._position[main_axis] - self._anchored_pos[main_axis] >= 2:  # NOQA
+                    self._current_delta = - 1
+            else:
+                if self._position == self._anchored_pos:
+                    self._current_delta = - 1
+                elif self._anchored_pos[main_axis] - self._position[main_axis] >= 2:  # NOQA
+                    self._current_delta = 1
 
-        self._timer += ticks
-        if self._timer > .2:
+            self._timer += ticks
+            if self._timer > .2:
 
-            # Convert to list so that we can easily change main axis
-            self._position = list(self._position)
-            self._position[main_axis] += self._current_delta
-            self._position = tuple(self._position)
+                # Convert to list so that we can easily change main axis
+                self._position = list(self._position)
+                self._position[main_axis] += self._current_delta
+                self._position = tuple(self._position)
 
-            self._timer -= .2
+                self._timer -= .2
